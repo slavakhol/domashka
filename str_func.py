a8a2f21ed785a25bb6d6d7a0341e31720d2a9b4d
Slava Kholopov <slava.kholopov@gmail.com>
2023-03-10 15:38:17 +0100

    added second fucntion

--- str_func.py
+++ str_func.py
@@ -1,3 +1,7 @@
 def capitalize(text):
     """ This function take the text and returns all symbols uppercase"""
     return text.upper()
+
+def capitzalize_first(text):
+    """ This function take the text and returns first symbols uppercase"""
+    return text.capitalize()
